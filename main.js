8a90d04f866770c76dfc10ec61afe367f5ecd32e
karim <kokekarim@yahoo.com>
2018-12-26 22:33:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//setup
+let fact = document.querySelector("#fact");
+let factText = document.querySelector("#factText");
+let numberInput = document.querySelector("#numberInput");
+numberInput.addEventListener("input", getFactAjax);
+
+// api call with Ajax
+function getFactAjax() {
+  let numberFact = numberInput.value;
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://numbersapi.com/" + numberFact);
+  xhr.onload = function() {
+    if (xhr.status == 200 && numberFact != "") {
+      fact.style.display = "block";
+      factText.innerText = this.responseText;
+    }
+  };
+
+  xhr.send();
+}
+
+// api call with fetch()
+function getFactFetch() {
+  let numberFact = numberInput.value;
+
+  fetch("http://numbersapi.com/" + numberFact)
+    .then(response => response.text())
+    .then(data => {
+      if (numberFact != "") {
+        fact.style.display = "block";
+        factText.innerText = data;
+      }
+    })
+    .catch(err => console.log("err"));
+}
